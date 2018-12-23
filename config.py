e4653d53e650e37a83a50a291ff7cbccaf24da8c
ShadeOfSadness <45738857+ShadeOfSadness@users.noreply.github.com>
2018-12-23 23:37:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 import requests
 from telebot import apihelper
-token = '783768962:AAFu1D9VkyjfdSFNXCSAwS2g-atvkjzp58Y'
+token = '**************************'
 apihelper.proxy = {"https": "socks5://104.238.97.44:6297"}
